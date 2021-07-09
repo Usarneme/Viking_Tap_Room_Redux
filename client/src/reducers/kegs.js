f78351c1fd20dc77f75b8ca42934ca45ba7e91d1
Usarneme <jamestcoop@gmail.com>
2021-07-09 09:16:36 -0700

    :white_check_mark: adds code to pass previous UPDATE_KEG test

--- client/src/reducers/kegs.js
+++ client/src/reducers/kegs.js
@@ -7,6 +7,17 @@ const kegReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD:
       return [...state, { id, brand, name, price, pintsRemaining, alcoholContent }]
+    case UPDATE:
+      const kegsWithoutUpdated = state.filter(k => k.id !== action.id)
+      const updatedKeg = {
+        id: action.id,
+        brand: action.brand,
+        name: action.name,
+        price: action.price,
+        pintsRemaining: action.pintsRemaining,
+        alcoholContent: action.alcoholContent
+      }
+      return [...kegsWithoutUpdated, updatedKeg]
     default:
       return state
   }
