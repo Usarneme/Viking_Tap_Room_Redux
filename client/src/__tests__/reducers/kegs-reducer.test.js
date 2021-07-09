98e4c7b4914f7a20a8d26206a0d1251b5c715881
Usarneme <jamestcoop@gmail.com>
2021-07-09 09:00:02 -0700

    :white_check_mark: adds a sanity check test for the keg reducer; empty state with no action should return empty array

--- client/src/__tests__/reducers/kegs-reducer.test.js
+++ client/src/__tests__/reducers/kegs-reducer.test.js
@@ -1,9 +1,34 @@
-const defaultState = require('./../../defaultKegs.json')
+import { kegs as defaultState } from './../../defaultKegs.json'
 // Redux action constants
 import { ADD, UPDATE, DELETE } from './../../actions/kegs'
+// Reducer
+import kegReducer from './../../reducers/kegs'
 
 describe('🍻 KEGS REDUCERS TESTS 🍻', () => {
+  let action
+  const testKeg = {
+    "id": "ace347c6-909e-4ebe-a61e-c51553ee479d",
+    "brand": "Viking Brews",
+    "name": "Test Beer",
+    "price": 6,
+    "pintsRemaining": 124,
+    "alcoholContent": 8
+  }
+
   test('sanity check', () => {
     expect(true).toEqual(true)
   })
+
+  test('an empty state array with no action should return an empty array', () => {
+    expect(kegReducer([],{ action: null })).toEqual([])
+  })
+
+  // test('it adds a new keg successfully upon dispatch of ADD_KEG action', () => {
+  //   action = {
+  //     ADD, ...testKeg
+  //   }
+  //   expect()
+  // })
 })
+
+
