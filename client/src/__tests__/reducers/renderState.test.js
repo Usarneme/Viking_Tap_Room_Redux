ee153b81964d0dc66c095bd5aa93e2d40ad36659
Usarneme <jamestcoop@gmail.com>
2021-07-09 11:52:53 -0700

    :test_tube: adds failing test to check for TOGGLE_EDIT action changing render state from false to true after being invoked once

--- client/src/__tests__/reducers/renderState.test.js
+++ client/src/__tests__/reducers/renderState.test.js
@@ -3,13 +3,25 @@ import { TOGGLE_EDIT } from './../../actions/renderState'
 import renderStateReducer from './../../reducers/renderState'
 
 describe('🎛  REACT RENDER STATE TESTS 🎛', () => {
+  let action
+  const defaultRenderState = { renderEditForm: false }
+
   test('sanity check', () => {
     expect(true).toEqual(true)
   })
 
-  const defaultRenderState = { renderEditForm: false }
-
   test('show edit form should default to false', () => {
     expect(renderStateReducer(defaultRenderState, { type: null })).toBe(defaultRenderState)
   })
+
+  test('show edit form action should toggle state from false to true', () => {
+    action = {
+      type: TOGGLE_EDIT
+    }
+    const resultRenderState = {
+      renderEditForm: true
+    }
+    expect(renderStateReducer(defaultRenderState, action)).toBe(resultRenderState)
+  })
+
 })
