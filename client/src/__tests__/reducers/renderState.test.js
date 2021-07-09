d25a266c40c976b18f324b5cb6f1b4fb428445cc
Usarneme <jamestcoop@gmail.com>
2021-07-09 11:46:57 -0700

    :test_tube: adds failing test for render state edit form default state

--- client/src/__tests__/reducers/renderState.test.js
+++ client/src/__tests__/reducers/renderState.test.js
@@ -1,8 +1,15 @@
 // Redux action constants
 import { TOGGLE_EDIT } from './../../actions/renderState'
+import renderStateReducer from './../../reducers/renderState'
 
-describe('🎛 REACT RENDER STATE TESTS 🎛', () => {
+describe('🎛  REACT RENDER STATE TESTS 🎛', () => {
   test('sanity check', () => {
     expect(true).toEqual(true)
+  })
+
+  const defaultRenderState = false
+
+  test('show edit form should default to false', () => {
+    expect(renderStateReducer(defaultRenderState, { type: null })).toBe(defaultRenderState)
   })
 })
